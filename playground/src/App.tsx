634e93c339172c9e068cceb9e38ec7c27887d204
hunghg255 <giahunghust@gmail.com>
2023-10-25 15:52:56 +0700

    chore: update example

--- playground/src/App.tsx
+++ playground/src/App.tsx
@@ -5,12 +5,32 @@ import b from './assets/images/b.png';
 
 function App() {
   return (
-    <div className='App'>
-      <img src='/images/a.jpeg' alt='a' />
-      <img src='/images/b.png' alt='a' />
+    <div className="App">
+      <img src="/images/a.jpeg" alt="a" />
+      <img src="/images/b.png" alt="a" />
 
-      <img src={a} alt='' />
-      <img src={b} alt='' />
+      <img src={a} alt="" />
+      <img src={b} alt="" />
+
+      <div
+        style={{
+          backgroundImage: `url(${a})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100px',
+          height: '100px',
+        }}
+      ></div>
+      <div
+        style={{
+          backgroundImage: `url('/images/a.jpeg')`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          width: '100px',
+          height: '100px',
+        }}
+      ></div>
+      <div className="img"></div>
     </div>
   );
 }
